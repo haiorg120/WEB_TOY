c0fe8414bb4f497f617a33845f98a7ccbdf6724a
haiorg120 <chubedan192001@gmail.com>
2024-10-07 09:17:23 +0700

    Initial commit

---
+++ data/products.js
@@ -0,0 +1,133 @@
+// data/products.js
+
+const products = [
+    {
+      "id": 1,
+      "name": "Robot Alpha",
+      "description": "Robot Alpha is designed for industrial automation with high precision and reliability.",
+      "price": "$3,500",
+      "image": "https://i.pinimg.com/564x/f1/7d/db/f17ddb244e3f2f6a720e61cd3f8161fb.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Robot Beta",
+      "description": "A versatile robot for various domestic applications, including cleaning and maintenance.",
+      "price": "$1,200",
+      "image": "https://i.pinimg.com/236x/e2/b1/45/e2b14504a109557cb744d3a9856987ae.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Robot Gamma",
+      "description": "An advanced robot equipped with AI capabilities for complex problem-solving tasks.",
+      "price": "$7,800",
+      "image": "https://i.pinimg.com/236x/dc/43/68/dc4368160e34d488a8c3a6c703ec017d.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Robot Delta",
+      "description": "Ideal for educational purposes, Robot Delta helps students learn about robotics and programming.",
+      "price": "$500",
+      "image": "https://i.pinimg.com/236x/fd/c8/f8/fdc8f8d6a56a2e33c3b4aa4c167c2572.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Robot Epsilon",
+      "description": "A high-end robot for research and development in advanced robotics.",
+      "price": "$10,000",
+      "image": "https://i.pinimg.com/236x/6d/45/b3/6d45b3573da9325786a8f51663d99d04.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Robot Zeta",
+      "description": "Zeta is designed for medical applications, providing precision in surgical procedures.",
+      "price": "$12,000",
+      "image": "https://i.pinimg.com/236x/8b/53/52/8b53529edfc7aac984da7a8e7edf101c.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Robot Eta",
+      "description": "Eta is a security robot that offers advanced surveillance capabilities.",
+      "price": "$4,500",
+      "image": "https://i.pinimg.com/236x/62/8d/de/628ddea8aa0abe5ee627b1a41ce4a407.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Robot Theta",
+      "description": "Theta is a companion robot designed to provide emotional support and interaction.",
+      "price": "$2,000",
+      "image": "https://i.pinimg.com/236x/e1/13/9e/e1139ed139bf965ffe95784b5aaf0052.jpg"
+    },
+    {
+      "id": 9,
+      "name": "Robot Iota",
+      "description": "A compact robot that specializes in tasks related to logistics and warehouse management.",
+      "price": "$8,500",
+      "image": "https://i.pinimg.com/236x/21/2e/5f/212e5f17978df344781061ab9f89b36d.jpg"
+    },
+    {
+      "id": 10,
+      "name": "Robot Kappa",
+      "description": "Kappa is engineered for agricultural use, automating planting and harvesting processes.",
+      "price": "$6,200",
+      "image": "https://i.pinimg.com/236x/79/e5/60/79e560ae9f7ae10a479b2d3f77b818d8.jpg"
+    },
+    {
+      "id": 11,
+      "name": "Robot Lambda",
+      "description": "Lambda is a humanoid robot that excels in human-robot interaction scenarios.",
+      "price": "$15,000",
+      "image": "https://i.pinimg.com/236x/36/e5/23/36e5238c45ac6679d9706f41a3eb105a.jpg"
+    },
+    {
+      "id": 12,
+      "name": "Robot Mu",
+      "description": "Mu is a multi-terrain robot designed for exploration and search and rescue missions.",
+      "price": "$9,000",
+      "image": "https://i.pinimg.com/236x/42/27/b9/4227b9fe4ccbed1ac08cd087388aae70.jpg"
+    },
+    {
+      "id": 13,
+      "name": "Robot Nu",
+      "description": "Nu is an underwater robot used for deep-sea exploration and data collection.",
+      "price": "$11,500",
+      "image": "https://i.pinimg.com/236x/09/b8/8d/09b88d21f070c54635a2bc0c3c079ddd.jpg"
+    },
+    {
+      "id": 14,
+      "name": "Robot Xi",
+      "description": "Xi is a robot designed for space exploration, capable of operating in extreme environments.",
+      "price": "$25,000",
+      "image": "https://i.pinimg.com/236x/36/9e/8e/369e8e5b35fadb71b80204ca03000a32.jpg"
+    },
+    {
+      "id": 15,
+      "name": "Robot Omicron",
+      "description": "Omicron specializes in entertainment, offering interactive features for amusement parks.",
+      "price": "$7,000",
+      "image": "https://i.pinimg.com/236x/7a/1e/fa/7a1efaaa794fef2c90ae6b9f808f95c9.jpg"
+    },
+    {
+      "id": 16,
+      "name": "Robot Pi",
+      "description": "Pi is a modular robot that can be customized for various industrial applications.",
+      "price": "$18,000",
+      "image": "https://i.pinimg.com/236x/97/19/70/971970605fc12e0a49676699bf2137fc.jpg"
+    },
+    {
+      "id": 17,
+      "name": "Robot Rho",
+      "description": "Rho is a robotics platform designed for robotics research and development.",
+      "price": "$13,500",
+      "image": "https://i.pinimg.com/236x/50/39/56/503956337a3fdbb4c0b6c1c1a7e44060.jpg"
+    },
+    {
+      "id": 18,
+      "name": "Robot Sigma",
+      "description": "Sigma is an all-purpose robot suitable for both domestic and commercial use.",
+      "price": "$5,200",
+      "image": "https://i.pinimg.com/236x/7d/2d/82/7d2d82344f750072083a176a1b368b77.jpg"
+    }
+  ];
+  
+  module.exports = products;
+  
